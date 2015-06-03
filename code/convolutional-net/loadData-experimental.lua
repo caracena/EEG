8433f7c13c7d11c8cb5d072c010c76bc0b327ff2
pablo <pablo.loyola.h@gmail.com>
2015-06-03 17:23:50 +0900

    mejoras

--- code/convolutional-net/loadData-experimental.lua
+++ code/convolutional-net/loadData-experimental.lua
@@ -1,5 +1,11 @@
 require 'torch'
 
+local filePath = '/home/ubuntu/EEG_data/s'
+local labelPath = '/home/ubuntu/EEG_labels/labels.csv'
+
+nusers = 1
+-------------------------
+
 function string:split(sep)
   local sep, fields = sep, {}
   local pattern = string.format("([^%s]+)", sep)
@@ -7,46 +13,38 @@ function string:split(sep)
   return fields
 end
 
---toy example
---local filePath = '/home/ubuntu/EEG/code/data.txt'
---real file
-local filePath = '/home/ubuntu/EEG_data/txt1/s01.txt'
+-- retorna 40 chunks(trials) de (8064x40)
+function loadUserData(path)
 
-
---Get rows, cols
-local nrows = 0 
-for line in io.lines(filePath) do
+ local nrows = 0
+ for line in io.lines(path) do
         ncols = #line:split(' ')
         nrows = nrows + 1
+ end
+
+ local data = torch.Tensor(nrows, ncols)
+
+ local i = 0
+ for line in io.lines(path) do
+        local l = line:split(' ')
+        for key, val in ipairs(l) do
+                data[i+1][key] = val
+        end
+        i = i + 1
+ end
+ local d = torch.Tensor(data)
+ trials  = d:chunk(40,2)
+ return trials
 end
 
-print(nrows)
-print(ncols)
 
-local data     = torch.Tensor(nrows, ncols)
+for i = 1,nusers do
+ if i <10 then
+  print(filePath .."0"..i..".txt" )
+ else
+  print(filePath..i..".txt" ) 
+ end
+end 
 
-local i = 0
-for line in io.lines(filePath) do
-	local l = line:split(' ')
-	for key, val in ipairs(l) do
-		data[i+1][key] = val
-   	end
-	i = i + 1
-end
-
-
---comando util para testing: 
---head -n 1 file.txt | cut -c-20
---print(data[1][1])
-print(data:size())
-
---segment:
-
-d = torch.Tensor(data)
-print(d:size()) 
-
-trials = d:chunk(40,2)
-
-print(trials[1]:size())
 
 
